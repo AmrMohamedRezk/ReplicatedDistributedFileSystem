cf16e264ac112786e1f4180dfa1770164672350c
eng.sengab@gmail.com <eng.sengab@gmail.com>
2014-05-24 10:27:16 +0300

    removing manipulation

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -204,4 +204,15 @@ public class Client {
 
 	}
 
+	public static void main(String[] args) throws RemoteException, IOException,
+			MessageNotFoundException {
+		Client c = new Client();
+		FileContent content = new FileContent("Ahmad.txt", 0);
+
+		WriteMsg m = c.write(null, content);
+		content = new FileContent("Ahmad.txt", m.getTransactionId());
+		content.setContent("I am testing :P :P ");
+		c.commit(m, 1);
+
+	}
 }
